5cda5d38cc22228fb9f71cd8858e21b290767104
sato wataru <watarusato105@gmail.com>
2024-07-11 15:33:35 +0900

    #1 eslint設定修正

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,6 +34,7 @@ module.exports = {
     'jsx-a11y',
     'react',
     'react-hooks',
+    'prefer-arrow',
     '@typescript-eslint'
   ],
   rules: {
@@ -96,6 +97,21 @@ module.exports = {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'prefer-arrow/prefer-arrow-functions': [
+      'error',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false,
+      },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
     ],
   },
   overrides: [
